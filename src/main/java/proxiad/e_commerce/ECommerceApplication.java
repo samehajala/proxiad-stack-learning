4ebc31a5d2237c66110d359393acab72ab536c14
samehajala <sameh.ajala@etudiant-enit.utm.tn>
2025-02-04 20:29:35 +0100

    Proxiad-stack-learning

---
+++ src/main/java/proxiad/e_commerce/ECommerceApplication.java
@@ -0,0 +1,13 @@
+package proxiad.e_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/proxiad/e_commerce/order/entities/Produit.java
@@ -0,0 +1,8 @@
+package proxiad.e_commerce.order.entities;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Produit {
+    
+}
